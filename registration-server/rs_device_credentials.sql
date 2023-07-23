68a53c464a785b27316e0e7a37cdbf06d4e7f47e
Alexander Castro <Alexcast129@gmail.com>
2023-07-23 08:18:14 -0700

    Renamed the RS server sql files

--- registration-server/rs_device_credentials.sql
+++ registration-server/rs_device_credentials.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS device_credentials (
     uuid VARCHAR(20) PRIMARY KEY,
     device_username VARCHAR(20) NOT NULL,
     device_password VARCHAR(20) NOT NULL,
-    UNIQUE(uuid, device_username, device_password)
+    device_ip_address VARCHAR(20) NOT NULL,
+    device_port INTEGER NOT NULL,
+    UNIQUE(uuid, device_username, device_password, device_ip_address, device_port)
 );
